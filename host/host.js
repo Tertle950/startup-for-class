7b621e274a97d678976145d2aae16eb8ff500d3c
Tertle950 <tertle950@gmail.com>
2024-02-28 09:44:48 -0800

    README update and self-hatred (so much left)

--- host/host.js
+++ host/host.js
@@ -2,8 +2,8 @@ const suggestions = document.getElementById("suggestions");
 
 // Simulate suggestions that will come over WebSocket
 setInterval(() => {
-    const message = "Medication for depression";
-    suggestions.innerHTML =
-      `<li>${message}</li>` +
-      suggestions.innerHTML;
-  }, 5000);
+  const message = "Medication for depression";
+  suggestions.innerHTML =
+    `<li>${message}</li>` +
+    suggestions.innerHTML;
+}, 5000);
