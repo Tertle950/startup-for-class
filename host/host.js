77e6662046f3343955290a8158b0ec976cad6037
Tertle950 <tertle950@gmail.com>
2024-02-29 04:12:38 -0800

    host.js room codes + license

--- host/host.js
+++ host/host.js
@@ -1,4 +1,23 @@
 const suggestions = document.getElementById("suggestions");
+const roomcode = document.getElementById("code");
+
+// https://stackoverflow.com/a/1349426
+function makecode(len) { // dynamic length may come in useful later
+  const characters = 'ABEFGHIJKLMNQRSTUWXYZ234679';
+  const charamount = characters.length;
+  let result = '';
+  let counter = 0;
+
+  while (counter < len) {
+    if (counter != 0 && counter % 3 == 0) result = result.concat('','-');
+    result = result.concat('',characters.charAt(Math.floor(Math.random() * charamount)));
+    counter += 1;
+  }
+
+  return result;
+}
+
+roomcode.innerHTML = makecode(8);
 
 // Simulate suggestions that will come over WebSocket
 setInterval(() => {
